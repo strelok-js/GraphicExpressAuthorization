71fe423f56a9093e021dc3dda5f72a5991f90c88
Strelok.js <strelok@justcoders.ru>
2024-09-15 00:10:01 +0300

    VI

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ class GraphicExpressAuthorization {
             }
 
             const authData = await this.config.authorization(req.body.login, req.body.password);
-            if(!authData||(authData.error&&!authData.login)) return res.status(401).json({message: authData.error??"Error identification"});
+            if(!authData||(authData.error&&!authData.login)) return res.status(401).json({message: authData?.error??"Error identification"});
             else {
                 res.cookie('jwtoken', this.generateJWT(authData.login, this.getPayload(authData)), this.config.cookie??{
                     path: '/',
@@ -76,7 +76,7 @@ class GraphicExpressAuthorization {
             httpOnly: true,
             sameSite: 'Strict'
         });
-        next();
+        return next();
     }
 }
 
