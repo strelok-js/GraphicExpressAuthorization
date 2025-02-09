df95649ce454f247a0a3d48954fdbc4789a7b356
Alexey <112818147+UmbrellaPix@users.noreply.github.com>
2025-02-09 14:29:26 +0300

    1

---
+++ main.ts
@@ -0,0 +1,63 @@
+import GraphicExpressAuthorization from './dist/index';
+
+const users: { [key: string]: string } = {
+    '123': '123',
+};
+
+// authorization function has to return payload with login or null
+
+const { graphicExpressAuthorization, router, identification } = new GraphicExpressAuthorization({
+    authorization: function authorization(login, password) {
+        if (!users[login]) return null;
+        if (users[login] === password) return { login, payload: '123', payload2: '123' };
+        return null;
+    },
+    bruteforce: {
+        // optional
+        delay: 5000, // delay between password entry
+    },
+    htmlPath: undefined, // if you need to use your own html, details below
+    jwt: {
+        privateKey: require('crypto').randomBytes(512).toString('hex'),
+        publicKey: undefined,
+        payload: ['payload', 'payload2'], // allowed payload from authorization function and JWT
+        genConfig: {
+            algorithm: 'HS256',
+            expiresIn: '20000',
+        },
+        genPrivateConfig: {
+            algorithm: 'HS256',
+            expiresIn: '7d',
+        },
+    },
+    cookie: {
+        // optional. Settings for the use of cookies
+        path: '/',
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+    },
+    authPath: '/api/', // have to be the same as api router path
+});
+
+import express from 'express';
+const app = express();
+import cookieParser from 'cookie-parser';
+
+app.use('/api/', router); // have to be the same as authPath
+
+app.use('/enter', cookieParser(), identification, (req, res) => {
+    // identification can't be global
+    res.json({ message: 'WELCOME' });
+});
+app.listen(3000, () => {
+    console.log(
+        ` ┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑` +
+            '\n' +
+            ` ┝━━┥ Server is started.` +
+            '\n' +
+            ` ┝━━┥ http://localhost:3000/enter` +
+            '\n' +
+            ` ┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙`,
+    );
+});
---
+++ src/index.ts
@@ -0,0 +1,190 @@
+import express, { Request, Response, NextFunction, Router, CookieOptions, request } from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import jwt, { JwtPayload, SignOptions } from 'jsonwebtoken';
+
+type JSONValue = string | number | boolean | null | JSONObject | JSONArray | undefined;
+
+interface JSONObject {
+    [key: string]: JSONValue;
+}
+
+interface JSONArray extends Array<JSONValue> {}
+
+interface JWTConfig {
+    privateKey: string;
+    publicKey?: string;
+    genConfig: jwt.SignOptions;
+    genPrivateConfig: jwt.SignOptions;
+    payload?: string[];
+}
+
+interface AuthorizationConfig {
+    htmlPath?: string;
+    authPath?: string;
+    jwt: JWTConfig;
+    cookie?: CookieOptions;
+    bruteforce?: { [key: string]: number };
+    authorization: (login: string, password: string) => Promise<JSONObject | null> | JSONObject | null;
+}
+
+export class GraphicExpressAuthorization {
+    private config: AuthorizationConfig;
+    router: Router;
+    lastLoginTime: Record<string, number>;
+    graphicExpressAuthorization: this;
+    identification: (req: Request, res: Response, next?: NextFunction) => Promise<void>;
+    GEA: GraphicExpressAuthorization;
+
+    constructor(config: AuthorizationConfig) {
+        this.config = config;
+        this.router = this.createRouter();
+        this.graphicExpressAuthorization = this.GEA = this;
+        this.identification = this.useIdentificationFunction.bind(this);
+        // this.identification.withGroup = this.identificationWithGroup.bind(this);
+
+        this.lastLoginTime = {};
+    }
+
+    private createRouter(): Router {
+        const router = express.Router();
+
+        router.use(bodyParser.json());
+        router.use(cookieParser());
+        router.get('/authentication.js', (req: Request, res: Response) => {
+            res.sendFile(__dirname + '/authentication.js');
+        });
+
+        router.post('/setJWT', async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const { login, password } = req.body;
+
+                if (this.config.bruteforce) {
+                    if (Date.now() - (this.lastLoginTime[login] ?? 0) <= this.config.bruteforce.delay) {
+                        res.status(429).json({ message: 'Identification attempt is too frequent' });
+                        return;
+                    }
+                    this.lastLoginTime[login] = Date.now();
+                }
+
+                const authData = await this.config.authorization(login, password);
+                if (!authData || (authData.error && !authData.login)) {
+                    res.status(401).json({ message: authData?.error ?? 'Error identification' });
+                    return;
+                }
+
+                const payload = this.getPayload(authData);
+                res.cookie(
+                    'jwtoken',
+                    this.generateJWT(payload, this.config.jwt.genConfig),
+                    this.config.cookie ?? {
+                        path: '/',
+                        secure: true,
+                        httpOnly: true,
+                        sameSite: 'strict',
+                    },
+                );
+                res.cookie(
+                    'refreshToken',
+                    this.generateJWT(payload, this.config.jwt.genPrivateConfig),
+                    this.config.cookie ?? {
+                        path: '/',
+                        secure: true,
+                        httpOnly: true,
+                        sameSite: 'strict',
+                    },
+                );
+
+                res.json({ message: 'Identification is successful' });
+            } catch (error) {
+                next(error);
+            }
+        });
+
+        router.use('/', this.useRefreshToken.bind(this), (req: Request, res: Response) => {
+            res.sendFile(this.config.htmlPath ?? __dirname + '/index.html');
+        });
+
+        return router;
+    }
+
+    getPayload(JWT: JSONObject) {
+        if (!this.config.jwt.payload) return undefined;
+        const out: JSONObject = {};
+        for (const key of this.config.jwt.payload) out[key] = JWT[key];
+        return out;
+    }
+
+    /*
+    public identificationWithGroup(group: string | string[]) {
+        const groups = Array.isArray(group) ? group : [group];
+        return async (req: Request, res: Response, next: NextFunction = () => {}) => {
+            const JWTGroups = await this.useIdentificationFunction(req, res, (data) => data?.groups ?? []);
+            if (!JWTGroups) return;
+            if (JWTGroups.some((el: string) => groups.includes(el))) {
+                return next(JWTGroups : undefined);
+            }
+            return res.redirect(
+                `${req.protocol}://${req.get('host')}${this.config.authPath}?old=${req.originalUrl}`
+            );
+        };
+    }
+*/
+    public async useIdentificationFunction(req: Request, res: Response, next: NextFunction = () => {}): Promise<void> {
+        const token = req.cookies.jwtoken;
+        const decodedJWT = token && (await this.validateJwt(token, this.config.jwt.publicKey || this.config.jwt.privateKey));
+
+        if (!decodedJWT) {
+            res.status(307)
+                .set('Location', `${req.protocol}://${req.get('host')}${this.config.authPath}?old=${req.originalUrl}`)
+                .end();
+            return;
+        }
+        return next();
+    }
+
+    private async useRefreshToken(req: Request, res: Response, next: NextFunction = () => {}): Promise<void> {
+        const token = req.cookies.refreshToken;
+        const decodedJWT = token && (await this.validateJwt(token, this.config.jwt.publicKey || this.config.jwt.privateKey));
+
+        if (!decodedJWT) {
+            return next();
+        }
+        res.cookie(
+            'jwtoken',
+            this.generateJWT(this.getPayload(decodedJWT), this.config.jwt.genConfig),
+            this.config.cookie ?? {
+                path: '/',
+                secure: true,
+                httpOnly: true,
+                sameSite: 'strict',
+            },
+        );
+
+        if (!req.query.old && typeof req.query.old !== 'string') {
+            res.status(400).json({ error: 'oldPath is not define' });
+            return;
+        }
+
+        res.status(307)
+            .set('Location', req.query.old as string)
+            .end();
+    }
+
+    private validateJwt(token: string, key: string): Promise<JwtPayload | null> {
+        return new Promise((resolve, reject) => {
+            jwt.verify(token, key, (err, decoded) => {
+                if (err) {
+                    resolve(null);
+                }
+                resolve(decoded as JwtPayload);
+            });
+        });
+    }
+
+    private generateJWT(payload: JSONObject | undefined = {}, config: SignOptions): string {
+        return jwt.sign(payload, this.config.jwt.privateKey, config);
+    }
+}
+
+export default GraphicExpressAuthorization;
